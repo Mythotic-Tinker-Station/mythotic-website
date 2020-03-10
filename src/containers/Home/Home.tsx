cc04b57568a1c53e621368ee4a13f40026bd2614
Mathew Antich <mifu@allfearthesentinel.net>
2020-03-10 21:57:49 +0800

    Adjusting homepage

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Box from '@material-ui/core/Box'
+
+import classes from './Home.module.css'
 
 class HomePage extends Component {
-    constructor(props: any) {
-        super(props)
-    }
     
     render() {
         return (
-            <div>
-                Homepage
-            </div>
+            <Fragment>
+                <Box className={classes.bannerbox}>
+                    BannerBox
+                </Box>
+                <Box>
+                    NewsBox
+                </Box>
+            </Fragment>    
         )
     }
 }
